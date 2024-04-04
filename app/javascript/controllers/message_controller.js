310e47aadb295aa62670fac110d60a298a6db628
Kender <kendermendoza591@outlook.com>
2024-04-04 17:06:57 +0200

    teset

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -3,9 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static targets = [ "name" ]
+  static values = {
+    url: String
+  }
+
+  connect() {
+    console.log(this.urlValue);
+  }
 
   greet() {
     console.log("Que la mame ", this.name);
+    console.log(this.urlValue);
   }
 
   get name() {
