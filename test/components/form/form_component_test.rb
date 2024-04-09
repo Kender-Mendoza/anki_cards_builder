6bba52d543c864e3615c98d66649a261eb8ded20
Kender <kendermendoza591@outlook.com>
2024-04-09 19:00:21 +0200

    added testcases for history and improve form components test

--- test/components/form/form_component_test.rb
+++ test/components/form/form_component_test.rb
@@ -7,19 +7,19 @@ class Form::FormComponentTest < ViewComponent::TestCase
     form_component = render_inline Form::FormComponent.new
 
     assert_selector form_selector
-    assert_equal(term_input, form_component.css('input.input').to_html)
+    assert_equal(term_input_html, form_component.css('input.input').to_html)
     assert_selector 'div.buttons.mt-2'
-    assert_equal(submit_button, form_component.css('input.button.is-primary.is-fullwidth').to_html)
+    assert_equal(submit_button_html, form_component.css('input.button.is-primary.is-fullwidth').to_html)
   end
 
   private
 
-  def term_input
+  def term_input_html
     '<input placeholder="What do you want to search?" class="input" ' +
       'data-entry-target="term" type="text" name="term" id="term">'
   end
 
-  def submit_button
+  def submit_button_html
     '<input type="submit" name="commit" value="Search" ' +
       'class="button is-primary is-fullwidth" data-disable-with="Search">'
   end
---
+++ test/components/history/history_component_test.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class History::HistoryComponentTest < ViewComponent::TestCase
+  test 'render history' do
+    history = entries(:one)
+    history_component = render_inline History::HistoryComponent.new(history:)
+
+    assert_selector 'div.column.is-half'
+    assert_selector 'div.notification.is-primary'
+    assert_equal(p_term_html, history_component.css('p.is-size-4.has-text-centered').to_html)
+    assert_equal(p_explanation_html, history_component.css('p:nth-child(2)').to_html)
+  end
+
+  def p_term_html
+    %(<p class="is-size-4 has-text-centered"> Great </p>)
+  end
+
+  def p_explanation_html
+    %(<p> <strong> noun: </strong> the explanation of... </p>)
+  end
+end
